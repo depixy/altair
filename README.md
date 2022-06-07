f33273d986d8259fbb8c396bee65bfab0fc0f69c
Joshua Avalon <git@avalon.sh>
2022-06-07 15:24:10 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# @depixy/altair
+
+[![License][license-badge]][license] [![GitHub][github-badge]][github] [![NPM][npm-badge]][npm] [![Conventional Commits][conventional-commits-badge]][conventional-commits]
+
+[Altair][altair] plugin for [Depixy][depixy]. It is included in Depixy.
+
+## Options
+
+See `DepixyAltairOptions`.
+
+[altair]: https://github.com/altair-graphql/altair
+[depixy]: https://github.com/depixy/depixy
+[conventional-commits]: https://conventionalcommits.org
+[conventional-commits-badge]: https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg
+[license]: ./LICENSE
+[license-badge]: https://img.shields.io/github/license/depixy/altair?label=License
+[npm]: https://www.npmjs.com/package/@depixy/altair
+[npm-badge]: https://img.shields.io/npm/v/@depixy/altair
+[github]: https://github.com/depixy/altair
+[github-badge]: https://img.shields.io/github/package-json/v/depixy/altair?label=GitHub
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import fastifyPlugin from "fastify-plugin";
+import fastifyStatic from "@fastify/static";
+import { getDistDirectory, renderAltair, RenderOptions } from "altair-static";
+
+export interface DepixyAltairOptions extends RenderOptions {
+  enable?: boolean;
+  /**
+   * URL to set as the server endpoint.
+   *
+   * By default is `/graphql`
+   */
+  endpointURL?: string;
+  /**
+   * URL to be used as a base for relative URLs and hosting needed static files.
+   *
+   * By default is `/altair/`
+   */
+  baseURL?: string;
+  /**
+   * Path in which Altair will be accesible.
+   *
+   * By default is `/altair`
+   */
+  path?: string;
+}
+
+const plugin = fastifyPlugin<DepixyAltairOptions>(
+  async (fastify, opts = {}) => {
+    const {
+      path = "/altair",
+      baseURL = "/altair/",
+      endpointURL = "/graphql",
+      ...renderOptions
+    } = opts;
+
+    await fastify.register(fastifyStatic, {
+      root: getDistDirectory(),
+      prefix: baseURL
+    });
+    const altairPage = renderAltair({ baseURL, endpointURL, ...renderOptions });
+    fastify.get(path, (_req, res) => {
+      res.type("text/html").send(altairPage);
+    });
+  },
+  {
+    name: "@depixy/altair",
+    fastify: "3.x"
+  }
+);
+
+export default plugin;
